d745d8387b559b1cf66ac9f1c577c0ab81e0f3c6
Devi Amaolo <deviamaolo@gmail.com>
2022-06-24 03:07:49 -0300

    creating firebase project

---
+++ src/components/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+export const auth = firebase.initializeApp({
+    apiKey: "AIzaSyC71sCZZOqOCgiYFhLcf7gOP3FYl0s8ri8",
+    authDomain: "chatapp-95349.firebaseapp.com",
+    projectId: "chatapp-95349",
+    storageBucket: "chatapp-95349.appspot.com",
+    messagingSenderId: "786710612235",
+    appId: "1:786710612235:web:f8a8dac404e5ada01e3a0e",
+    measurementId: "G-3LJP6WY94F"
+}).auth();
